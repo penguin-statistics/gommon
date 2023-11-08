8dbf0e678ff4e6bef82fc2647866670f4ca94356
GalvinGao <me@galvingao.com>
2023-11-08 16:29:34 -0500

    chore: update report idempotency lifetime to 4

--- constant/report.go
+++ constant/report.go
@@ -23,7 +23,7 @@ const (
 	ViolationReliabilityRejectRuleRangeLeast = 1 << 8
 	ViolationReliabilityRejectRuleRangeMost  = 1 << 10
 
-	ReportIdempotencyLifetime     = time.Hour * 6
+	ReportIdempotencyLifetime     = time.Hour * 4
 	ReportIdempotencyRedisHashKey = "report-idempotency"
 
 	RecruitStageID  = "recruit"
